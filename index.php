b17f34fe3d8f29ba106b47647caa5bcde01027bd
SwipeShot <helplesson@gmail.com>
2015-04-24 17:34:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
-﻿<?php
-	include "ban.php";
-	
-	$time = date("j.n.Y G:i:s");
-	
-	$h=fopen("sssswwww.txt","at");
-	fwrite($h,"========\r\n$viewerId - $time\r\n");
-	fclose($h);
-	
-	if($_REQUEST["viewer_id"] == 0){
-		exit("Пожалуйста, загрузите эту страницу через <a href=\"//vk.com/app4884392\">vk.com/app4884392</a>");
-	};
-	
-	if(!$_SERVER['HTTP_REFERER'] == 'anon.swipeshot.ru/?send=message') {
-		exit('Пожалуйста, загрузите эту страницу через <a href="//vk.com/app4884392">vk.com/app4884392</a>');
-	}
-	else {
-	}
-?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
@@ -33,14 +15,7 @@
       var msg   = $('#form').serialize();
         $.ajax({
           type: 'POST',
-		  <?php
-			if($_REQUEST["viewer_id"] != 49656121){
-				echo "url: 'send.php',";
-			}
-			else{
-				echo "url: 'admin.php',";
-			}
-		  ?>
+	  url: 'http://anon.swipeshot.ru/send.php',
           data: msg,
           success: function(data) {
             $('.results').html(data);
@@ -57,27 +32,10 @@
 <title>Анонимное сообщение ВК</title>
 </head>
 <body>
-<?php
-	if($_REQUEST["viewer_id"] == 49656121){
-		echo "<header><a href=\"_adminopop.php\" target=\"_blank\">Админка</a></header>";
-	}
-	else{
-		
-	};
-?>
 <div class="wrap">
 <form id="form" method="POST" action="javascript:void(null);" onsubmit="call()">
 	<input value="" name="id" placeholder="ID. Только цифры" type="text" required/><br/>
 	<textarea name="message" placeholder="Что отправляем" required></textarea><br/>
-	<?php
-		if($_REQUEST["viewer_id"] != 49656121){
-			echo "<input type=\"text\" name=\"kapcha\" placeholder=\"Капча\" required/><br/>";
-			echo "<img src = \"captcha.php\"/>";
-		}
-		else{
-			
-		};
-	?>
 	<input type="submit" value="Отправить"/>
 </form>
 <div id="loader"><svg class="circle-loader progress" width="40" height="40" version="1.1" xmlns="http://www.w3.org/2000/svg">
